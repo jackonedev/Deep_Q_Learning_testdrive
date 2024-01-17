5e8070c0fb195dfb6b4bdc7b9a594b221f013be7
unknown <jackone.action.software@gmail.com>
2024-01-17 16:12:26 -0300

    updated download paths

--- 03_first_CNN/main_train.py
+++ 03_first_CNN/main_train.py
@@ -24,13 +24,13 @@ from main_feed import DATASET, DATA_STORAGE
 class SCALERS(Enum):
     NONE = None
     DEFAULT = "minmax"
-    STANDARD = StandardScaler
+    STANDARD = "standard"
 
 
 
-DENSE_LAYERS = [1]
+DENSE_LAYERS = [2]
 LAYER_SIZE = [64]
-CONV_LAYERS = [3]
+CONV_LAYERS = [2]
 
 
 
@@ -38,13 +38,13 @@ MODEL_NAME = f"cnn_01_a1_{int(time())}"
 SCALER = SCALERS.DEFAULT.value
 random_state = 2024
 workers = 4
-EPOCHS = 100
-BATCH_SIZE = 64
+EPOCHS = 30
+BATCH_SIZE = 32
 # LEARNING_RATE = 0.0045
 # MOMENTUM = 0.777
 
  
-def main():
+def main(verbose=False):
     from cnn_01_a1 import create_models as cm1
 
     # Loading the data
@@ -62,17 +62,24 @@ def main():
     if SCALER == SCALERS.NONE.value:
         # In favor of DRY we should have a scaler that does nothing only for defining a "scaler"
         pass
-    elif SCALER == SCALERS.DEFAULT.value:
+    elif SCALER == "minmax":
         X_train = keras.utils.normalize(X_train)
         X_val = keras.utils.normalize(X_val)
         X_test = keras.utils.normalize(X_test)
-    elif SCALER == SCALERS.STANDARD.value:
+    elif SCALER == "stardard":
         scaler = CustomScaler(scaler=StandardScaler)
         X_train = scaler.fit_transform(X_train)
         X_val = scaler.transform(X_val)
         X_test = scaler.transform(X_test)
 
-    
+    if verbose:
+        print("Shapes:")
+        print(f"X_train.shape: {X_train.shape}")
+        print(f"X_val.shape: {X_val.shape}")
+        print(f"X_test.shape: {X_test.shape}")
+        print(f"y_train.shape: {y_train.shape}")
+        print(f"y_val.shape: {y_val.shape}")
+        print(f"y_test.shape: {y_test.shape}")
     
     
     
@@ -94,9 +101,9 @@ def main():
         # UPDATE FILENAMES
         MODEL_BASE_NAME = f"{next(x):02}_{model_name}_{DATASET}"
         
-        MODELS_DIRNAME = f"models/{MODEL_BASE_NAME}.keras"
-        HISTORIES_DIRNAME = f"histories/{MODEL_BASE_NAME}.pkl"
-        LOGS_DIR = f"logs/{MODEL_BASE_NAME}"
+        MODELS_DIRNAME = f"models/{DATASET}/{MODEL_BASE_NAME}.keras"
+        HISTORIES_DIRNAME = f"histories/{DATASET}/{MODEL_BASE_NAME}.pkl"
+        LOGS_DIR = f"logs/{DATASET}/{MODEL_BASE_NAME}"
     
         # Training the model
         history = model.fit(
@@ -121,14 +128,14 @@ def main():
         
         # Saving the history
         os.makedirs(HISTORIES_DIRNAME.split("/")[0], exist_ok=True)
-        
+    
         with open(HISTORIES_DIRNAME, "wb") as f:
             pickle.dump(history.history, f)
             
 
 if __name__ == "__main__":
     
-    m = multiprocessing.Process(target=main)
+    m = multiprocessing.Process(target=main, args=(True,))
     
     #start the program
     m.start()   
@@ -139,4 +146,3 @@ if __name__ == "__main__":
     print("Done!")
 
 
-# TODO: create a RandomSearch for the hyperparameters
